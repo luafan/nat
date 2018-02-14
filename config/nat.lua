cdbe4e42eae5e3a0acf089278eb4ea55558ffaa5
Sam Chang <sam.chang@me.com>
2018-02-14 15:39:52 +0800

    add udp_mtu config.

--- config/nat.lua
+++ config/nat.lua
@@ -10,6 +10,8 @@ name = os.getenv("NAT_NAME")
 udp_package_timeout = tonumber(os.getenv("UDP_PACKAGE_TIMEOUT") or 3)
 udp_waiting_count = tonumber(os.getenv("UDP_WAITING_COUNT") or 100 * udp_package_timeout)
 
+udp_mtu = tonumber(os.getenv("UDP_MTU") or 576)
+
 keepalive_delay = tonumber(os.getenv("KEEPALIVE_DELAY") or 10)
 peer_timeout = tonumber(os.getenv("PEER_TIMEOUT") or 600)
 none_peer_timeout = tonumber(os.getenv("NONE_PEER_TIMEOUT") or 10)
